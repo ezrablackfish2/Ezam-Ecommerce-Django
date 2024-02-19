b4a7ac0dc25c5b3a450b7d63c163df5d3e8f95aa
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-02-19 16:39:00 +0300

    price had a minimum of 7 digits

--- base/models.py
+++ base/models.py
@@ -42,11 +42,11 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
     taxPrice = models.DecimalField(
-        max_digits=7, decimal_places=2, null=True, blank=True)
+        max_digits=20, decimal_places=2, null=True, blank=True)
     shippingPrice = models.DecimalField(
-        max_digits=7, decimal_places=2, null=True, blank=True)
+        max_digits=20, decimal_places=2, null=True, blank=True)
     totalPrice = models.DecimalField(
-        max_digits=7, decimal_places=2, null=True, blank=True)
+        max_digits=20, decimal_places=2, null=True, blank=True)
     isPaid = models.BooleanField(default=False)
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(default=False)
@@ -65,7 +65,7 @@ class OrderItem(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     qty = models.IntegerField(null=True, blank=True, default=0)
     price = models.DecimalField(
-        max_digits=7, decimal_places=2, null=True, blank=True)
+        max_digits=20, decimal_places=2, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
@@ -81,7 +81,7 @@ class ShippingAddress(models.Model):
     postalCode = models.CharField(max_length=200, null=True, blank=True)
     country = models.CharField(max_length=200, null=True, blank=True)
     shippingPrice = models.DecimalField(
-        max_digits=7, decimal_places=2, null=True, blank=True)
+        max_digits=20, decimal_places=2, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
